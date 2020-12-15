b9d73ae10a06f8ffe21d7282c39f86bc34e8ad73
Mario Stehling <mario.stehling@gmx.de>
2020-12-15 13:51:25 +0100

    Completed function-calling-function exercise

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -27,6 +27,17 @@
 // console.log(percentageOfWorld2(330));
 
 const percentageOfWorld3 = population => (population / 7900) * 100;
-console.log(percentageOfWorld3(1441));
-console.log(percentageOfWorld3(83));
-console.log(percentageOfWorld3(330));
+// function percentageOfWorld3(population) {
+//   return (population / 7900) * 100;
+// }
+// console.log(percentageOfWorld3(1441));
+// console.log(percentageOfWorld3(83));
+// console.log(percentageOfWorld3(330));
+
+// function describePopulation(country, population) {
+//   return `${country} has ${population} million people, which is about ${percentageOfWorld3(population)}% of the world.`;
+// }
+const describePopulation = (country, population) => `${country} has ${population} million people, which is about ${percentageOfWorld3(population)}% of the world.`;
+console.log(describePopulation("China", 1441));
+console.log(describePopulation("Germay", 83));
+console.log(describePopulation("USA", 330));

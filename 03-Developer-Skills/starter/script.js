16f1d33ab5a9c56162608af91c974e1a5cb7c814
Mario Stehling <mario.stehling@gmx.de>
2020-12-18 20:34:35 +0100

    🏆  Completed coding challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+////////////////////////////////////////////////
+// Coding challenge #1
+
+const printForecast = function (arr) {
+  let outputString = '';
+  for (let i = 0; i < arr.length; i++) {
+    outputString += `... ${arr[i]}°C in ${i + 1} days `;
+  }
+  outputString += '...';
+  console.log(outputString);
+};
+
+printForecast([12, 5, -5, 0, 4]);
+////////////////////////////////////////////////

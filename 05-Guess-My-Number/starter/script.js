0673eb0be5dfc00bca0d364024374a1bde57b522
Mario Stehling <mario.stehling@gmx.de>
2020-12-19 23:45:30 +0100

    Setup game structure

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// document.querySelector('.number').textContent;
+// document.querySelector('.score').textContent;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '🛑 No number entered!';
+  }
+});

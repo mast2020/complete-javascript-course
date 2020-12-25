880c6a398ce2e1c587f245b2e9dc5135aa9f37db
Mario Stehling <mario.stehling@gmx.de>
2020-12-25 13:19:21 +0100

    Created sample code to illustrate hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,51 +1,82 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// // --- SCOPE AND SCOPE CHAIN ---
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-      // firstName in current scope is a NEW variable with the same name as outer scope's variable,
-      // i.e. this new variable will take precedence over outer scope's variable in the variable lookup
-      // and str will include Steven, not Jonas
-      const firstName = 'Steven';
-      const str = `Oh, and you are a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
 
-      function add(a, b) {
-        return a + b;
-      }
+//       // firstName in current scope is a NEW variable with the same name as outer scope's variable,
+//       // i.e. this new variable will take precedence over outer scope's variable in the variable lookup
+//       // and str will include Steven, not Jonas
+//       const firstName = 'Steven';
+//       const str = `Oh, and you are a millenial, ${firstName}`;
+//       console.log(str);
 
-      // Re-assigning outer scope's variable
-      output = 'NEW OUTPUT';
-    }
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-    // var variables are only function-, but not block-scoped
-    console.log(millenial);
+//       // Re-assigning outer scope's variable
+//       output = 'NEW OUTPUT';
+//     }
 
-    // the block has access to the output variable in its parent scope, hence it can
-    // reassign its value; i.e. this will log 'NEW OUTPUT' instead of 'Jonas, you are ...'
-    console.log(output);
+//     // var variables are only function-, but not block-scoped
+//     console.log(millenial);
 
-    // const and let variables are block-scoped; this does NOT work:
-    // console.log(str);
+//     // the block has access to the output variable in its parent scope, hence it can
+//     // reassign its value; i.e. this will log 'NEW OUTPUT' instead of 'Jonas, you are ...'
+//     console.log(output);
 
-    // functions are also block-scoped in strict mode, i.e. only accessible inside the block;
-    // (if strict mode is turned off, functions are function-scoped); this does NOT work:
-    // console.log(add(2, 3));
-  }
+//     // const and let variables are block-scoped; this does NOT work:
+//     // console.log(str);
 
-  printAge();
-  return age;
+//     // functions are also block-scoped in strict mode, i.e. only accessible inside the block;
+//     // (if strict mode is turned off, functions are function-scoped); this does NOT work:
+//     // console.log(add(2, 3));
+//   }
+
+//   printAge();
+//   return age;
+// }
+
+// // firstName needs to be declared before the calcAge function call
+// // in order for firstName to 1) be included in the global execution context
+// // variable environment and 2) thus be accessible from within the calcAge function
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+// --- HOISTING ---
+
+// Variables:
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = '1991';
+
+// Functions
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-// firstName needs to be declared before the calcAge function call
-// in order for firstName to 1) be included in the global execution context
-// variable environment and 2) thus be accessible from within the calcAge function
-const firstName = 'Jonas';
-calcAge(1991);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;

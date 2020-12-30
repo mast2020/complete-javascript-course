7cb0078a8d77d7e061a388a900d4e843a5711537
Mario Stehling <mario.stehling@gmx.de>
2020-12-30 20:54:22 +0100

    Created sample code to illustrate REST operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -34,12 +34,41 @@
 
 // Avoiding argument-order-dependency
 
-const add = function (args) {
-  return args.a + args.b;
-};
-console.log(add({ a: 5, b: 3 }));
+// const add = function (args) {
+//   return args.a + args.b;
+// };
+// console.log(add({ a: 5, b: 3 }));
 
-const add2 = function ({ a, b }) {
-  return a + b;
+// const add2 = function ({ a, b }) {
+//   return a + b;
+// };
+// console.log(add2({ a: 5, b: 3 }));
+
+// REST and objects
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 };
-console.log(add2({ a: 5, b: 3 }));
+
+const { fri, ...remainder } = restaurant.openingHours;
+console.log(fri);
+console.log(remainder);

77e25f99283f6420ad79334f9709b306cd41fa58
Mario Stehling <mario.stehling@gmx.de>
2020-12-31 16:53:10 +0100

    🏆 Completed coding challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -46,29 +46,101 @@
 
 // REST and objects
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
+
+// const { fri, ...remainder } = restaurant.openingHours;
+// console.log(fri);
+// console.log(remainder);
+
+// --- CODING CHALLENGE 1 ---
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
 
-const { fri, ...remainder } = restaurant.openingHours;
-console.log(fri);
-console.log(remainder);
+// Task 1:
+const players1 = [...game.players[0]];
+const players2 = [...game.players[1]];
+
+// Task 2:
+const [gk, ...fieldPlayers] = players1;
+
+// Task 3:
+const allPlayers = [...players1, ...players2];
+
+// Task 4:
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// Task 5:
+const { team1: team1, x: draw, team2: team2 } = game.odds;
+
+// Task 6:
+const printGoals = function (...playerNames) {
+  for (let i = 0; i < playerNames.length; i++) {
+    console.log(playerNames[i]);
+  }
+  console.log(`Total number of goals: ${playerNames.length}`);
+};
+
+printGoals(...game.scored);
+
+// Task 7:
+console.log((team1 < team2 && 'team1') || 'team2');

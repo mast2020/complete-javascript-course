1f7064084ba73602bafaa27ebb07569c8489c2a1
Mario Stehling <mario.stehling@gmx.de>
2021-02-22 12:45:06 +0100

    🏆 Completed coding challenge 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,21 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// CODING CHALLENGE 1
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCopy = dogsJulia.slice(1, -2);
+  const mergedArray = dogsJuliaCopy.concat(dogsKate);
+  mergedArray.forEach(function (age, index) {
+    console.log(
+      age < 3
+        ? `Dog number ${index + 1} is still a puppy`
+        : `Dog number ${index + 1} is an adult, and is ${age} years old`
+    );
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log('----------');
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
